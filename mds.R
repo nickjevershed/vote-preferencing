26b4212915c6162048acad9ab334509988b8384b
Matthew Landauer <matthew@openaustralia.org>
2013-08-24 13:51:12 +1000

    Save plot to svg

--- mds.R
+++ mds.R
@@ -1,12 +1,19 @@
 # Run this within R with:
 # source("mds.R")
 
+graph <- function(fit) {
+  x <- fit$points[,1]
+  y <- fit$points[,2]
+  plot(x, y, type="n")
+  text(x, y, labels = row.names(d2), cex=.5)
+}
+
 d = read.table("distance_nsw.dat", header=TRUE)
 # Make the matrix symmetric. Simplistic - puts equal weight on preferencing in both directions
 d2 = d + t(d)
 fit <- cmdscale(d2, eig=TRUE, k=2)
-x <- fit$points[,1]
-y <- fit$points[,2]
-plot(x, y, type="n")
-text(x, y, labels = row.names(d2), cex=.5)
+graph(fit)
+svg("nsw.svg", width=7, height=7)
+graph(fit)
+dev.off()
 write.csv(fit$points, 'nsw-coords.csv')
