05250be4dca6af65bce5f7e94516503bd46f3111
Matthew Landauer <matthew@openaustralia.org>
2013-08-24 15:41:48 +1000

    More plot tweaks

--- mds.R
+++ mds.R
@@ -7,26 +7,25 @@ graph <- function(fit, d2, state) {
   x <- fit$points[,1]
   y <- fit$points[,2]
   plot(x, y, col="red", main=state, xlab="", ylab="", xaxt='n', yaxt='n', frame.plot=FALSE)
-  pointLabel(x, y, labels = row.names(d2), cex=.75, xpd=FALSE)
+  pointLabel(x, y, labels = row.names(d2), cex=1, xpd=FALSE)
 }
 
-process <- function(state) {
+process <- function(state, label) {
   d = read.table(sprintf("output/distance_%s.dat", state), header=TRUE)
   # Make the matrix symmetric. Simplistic - puts equal weight on preferencing in both directions
   d2 = d + t(d)
   fit <- cmdscale(d2, eig=TRUE, k=2)
-  #graph(fit, d2, state)
   svg(sprintf("output/%s.svg", state), width=7, height=7)
-  graph(fit, d2, state)
+  graph(fit, d2, label)
   dev.off()
   write.csv(fit$points, sprintf("output/%s-coords.csv", state))  
 }
 
-process("act")
-process("nsw")
-process("nt")
-process("qld")
-process("sa")
-process("tas")
-process("vic")
-process("wa")
+process("act", "ACT")
+process("nsw", "NSW")
+process("nt", "NT")
+process("qld", "QLD")
+process("sa", "SA")
+process("tas", "TAS")
+process("vic", "VIC")
+process("wa", "WA")
